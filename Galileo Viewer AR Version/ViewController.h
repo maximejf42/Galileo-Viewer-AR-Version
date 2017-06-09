c04f011f143f1c369e682fe8d94afdfa67977e18
robzim <Rob Zimmelman>
2017-06-09 11:39:32 -0400

    Initial Commit

---
+++ Galileo Viewer AR Version/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Galileo Viewer AR Version
+//
+//  Created by Robert Zimmelman on 6/9/17.
+//  Copyright © 2017 Robert Zimmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Galileo Viewer AR Version/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Galileo Viewer AR Version
+//
+//  Created by Robert Zimmelman on 6/9/17.
+//  Copyright © 2017 Robert Zimmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
